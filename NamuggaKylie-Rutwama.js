753fbd5afa961cb9bf44929abfbb91188474fa5c
Namugga Kylie <kylienamugga@gmail.com>
2022-02-15 16:02:51 +0300

    adds assignment

---
+++ NamuggaKylie-Rutwama.js
@@ -0,0 +1,68 @@
+let x = 5;
+let y = 3;
+
+//This is an addition operator instructing the computer to add the value of x and y//
+console.log('x + y = ', x + y);  // 8
+
+// This is a subtraction operator  instructing the computer to subtract the value of x and y//
+console.log('x - y = ', x - y);  // 2
+
+// This is a multiplication operator which multiplies the the value of x and y//
+console.log('x * y = ', x * y);  // 15
+
+// This is a division operator that divides the value of x and y//
+console.log('x / y = ', x / y);  // 1.6666666666666667
+
+// This is a modulous/Remainder operator//
+console.log('x % y = ', x % y);   // 2
+
+// increment operators//
+
+//This is an icreament operator that adds 1 to the value of x before running//
+console.log('++x = ', ++x); // 6
+
+//This is an increament operator that add 1 to the after running//
+console.log('x++ = ', x++); 6
+
+// This is the new value of x variable//
+console.log('x = ', x);     // 7
+
+
+// Decreament Operators//
+
+//This example subtracts one from the value of x before //
+console.log('--x = ', --x); // 6
+
+//This example subtracts one from the value of x after//
+console.log('x-- = ', x--); // 6
+
+//This is the new x value after being submitted to the decreamemt operator//
+console.log('x = ', x);     // 5
+
+//This is an exponetial operator, so its x value to the power of y value//
+console.log('x ** y =', x ** y); 125
+
+//These are comparison opperators//
+
+//This example is an equal operator, two equal signs mean "equal to"//
+console.log(2 == 2); // true
+
+console.log(2 == '2'); // true
+
+// This example means 3 is not equal to 2//
+console.log(3 != 2); // true
+
+//This means 'hello' is not equal to 'Hello'
+console.log('hello' != 'Hello'); // true
+
+// three equal signs represent a strict operator which means both variables must be equal in value and data type
+console.log(2 === 2); // true
+
+//This a false because much as the values are the same, the data type is differnt. Strict operator applied//
+console.log(2 === '2'); // false
+
+// This means not equal//
+console.log(2 !== '2'); // true
+
+//this is false because the example is equal both in data type and value//
+console.log(2 !== 2); // false
