452b00b6f2228453a729ed290d7ccbc1ff4ce681
kenyonyozi <kenyonyozibwengye@gmail.com>
2022-02-15 19:02:13 +0300

    Adds kenyonyozi.js file

---
+++ kenyonyozi.js
@@ -0,0 +1,60 @@
+let x = 5;
+let y = 3;
+
+// addition assignment because its telling the comupter add x and y values
+console.log('x + y = ', x + y);  // 8
+
+// subtraction assignment  because its telling the computer to subtrate x and y values
+console.log('x - y = ', x - y);  // 2
+
+// multipulication assignment because its telling the computer to multiply x and y values
+console.log('x * y = ', x * y);  // 15
+
+// division assignment becuse its telling the computer to divede x and y values
+console.log('x / y = ', x / y);  // 1.66666667
+// remainder operator / modulous operator becuse its telling computer to find the remainder of x and y values
+console.log('x % y = ', x % y);   // 2
+
+// increment operators
+//telling the computer to add 1 before x value
+console.log('++x = ', ++x); // 6
+//telling the computer to add 1 after (later) x value
+console.log('x++ = ', x++); // 6
+//telling the computer to make he value of x 7 after one has been added to it
+console.log('x = ', x);     // 7
+
+// arithementic operators
+//telling the computer to subtract 1 before from the value of x 
+console.log('--x = ', --x); // 6
+// telling the computer to subtract 1 later from the value x
+console.log('x-- = ', x--); // 6
+//tellung the computer to give us the value of 5
+console.log('x = ', x);     // 5
+
+//exponational operators
+//telling the computer to make the power of x value y eg x power 3 which is the value of y
+console.log('x ** y =', x ** y);
+
+//equal operators
+//this is because the values are the same/ equal eg both are numbers
+console.log(2 == 2); // true
+//this means equal to because the values are the same eg the string is 2 and the is a number 2
+console.log(2 == '2'); // true
+
+// comparision operators
+//this is true because the value 3 is not equal to 2
+console.log(3 != 2); // true
+//this is true because the data are not equal 
+console.log('hello' != 'Hello'); // true
+
+// strictly equal operator
+// this is true because values are strictly equal in data type and value 
+console.log(2 === 2); // true
+//this is false because the values and data type are not strictly equal eg one is number the other a string
+console.log(2 === '2'); // false
+
+// strictly not equal
+//this is true because the values are strictly not equal
+console.log(2 !== '2'); // true
+//this false because the two values are strictly 
+console.log(2 !== 2); // false
