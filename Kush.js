217f96d607c24764db99095b46649bd614cac10a
kushmuhamed <98839091+kushmuhamed@users.noreply.github.com>
2022-02-15 21:28:07 +0300

    Kush Muhamed

---
+++ Kush.js
@@ -0,0 +1,79 @@
+let x = 5;
+let y = 3;
+
+// adition operator
+// used for getting the sum of 2 operands in an expression
+// the answer for this will be 5+3=8
+console.log('x + y = ', x + y);  // 
+
+// subtraction operator
+//used in subtraction of 2 operands inorder to get a value .
+// answer will be 5-3 =2
+console.log('x - y = ', x - y);  // 
+
+// multiplication
+//used as the normal way of multipying two intergers to get a value
+//answer will be 5*3=15
+console.log('x * y = ', x * y);  // 
+
+// division operator 
+//used to divide two operands to get an answer 
+// answer will be 5/3= 1.6
+console.log('x / y = ', x / y);  // 
+// modulus operator 
+// returns the remainder of an interger wen x is divided by y
+// the answer here is 2 as its the remainder of 5/3 
+console.log('x % y = ', x % y);   // 
+
+// increment
+console.log('++x = ', ++x); // adds one to the original value
+// and returns a new value immediately.
+//the answer will be 6
+
+console.log('x++ = ', x++); // 
+//returns the original value before incrementing
+//the answer will be 6 
+console.log('x = ', x);     //
+//thats the original value of x 
+
+// 
+console.log('--x = ', --x); // substracts one to the original value
+// and returns a new value immediately.
+//answer will be 4
+console.log('x-- = ', x--); //
+//answer will be 4 
+//returns the original value before incrementing
+
+console.log('x = ', x);     // 
+//thats the original value of x 
+
+//
+console.log('x ** y =', x ** y);
+// this means x to the power of y
+// answer will be 5^3 = 125
+//
+console.log(2 == 2); // 
+// means 2 is equal to 2 which is true
+console.log(2 == '2'); // 
+// means equal even though they are not of the same type
+
+
+// 
+console.log(3 != 2); //
+//true since they are of differnt values  
+console.log('hello' != 'Hello'); //
+//true even though they are strings they are written differently 
+
+// 
+console.log(2 === 2); // true because they are of the same type
+console.log(2 === '2'); // false
+// they are of diffferent types as one is a number and other string
+
+// 
+console.log(2 !== '2'); // true
+//since they are different data types ie one is a string and other number.
+
+console.log(2 !== 2); // false
+// they are of the same data type and value 
+
+
