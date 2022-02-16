d726db050d4f1f9bca3d1be6635104460cceed09
Alwoch Sophia <83899148+Alwoch@users.noreply.github.com>
2022-02-16 08:55:03 +0300

    javascript Assignment1

---
+++ sophia-Alwoch.js.js
@@ -0,0 +1,48 @@
+let x = 5;
+let y = 3;
+
+// The above code initialises a variable x to a value 5 and Y to a value 3
+
+//Addition operator
+console.log("x + y = ", x + y); // log to the console a string "x+y=" and the value resulting from the addition of x and y
+
+// subtracton operator
+console.log("x - y = ", x - y); //  log to the console a string "x-y=" and the value resulting from the subtraction of y from x
+
+// multiplication operator
+console.log("x * y = ", x * y); // log to the console a string "x*y=" and the value resulting from the multiplication of x and y
+
+// division operator
+console.log("x / y = ", x / y); //  log to the console a string "x/y=" and the value resulting from the division of x by y
+
+// modulus operator
+console.log("x % y = ", x % y); // log to the console x modulus y and the value
+
+// increment
+console.log("++x = ", ++x); //  log to the console a string "++x=" and the value of icrement  of x (increment by 1 first and then add x)
+console.log("x++ = ", x++); //  lhttps://github.com/COHORT-10/Alwoch-Sophia.git
+console.log("x = ", x); // log to the console a string "x" and the value of x
+
+//Decrecement operator
+console.log("--x = ", --x); // log to the console a string "--x" and the value of the decrement of x by 1 (decrement by 1 first)
+console.log("x-- = ", x--); // log to the console a string "x--" and the value of the decrement of x by 1 (value of x and decrement by 1 later)
+console.log("x = ", x); // log to the console the string "x=" and the value of x
+
+//multipication operator
+console.log("x ** y =", x ** y); //log to the console a string "x**y=" and the value resulting from the multiplication of x and y
+
+//Equality operator
+console.log(2 == 2); // 2 is equal to 2
+console.log(2 == "2"); // 2 (number) is equal to 2 (string)
+
+//Not equal operator
+console.log(3 != 2); // 3 is not equal to 2
+console.log("hello" != "Hello"); // "hello" is not equal to "Hello"
+
+// strict equality operator
+console.log(2 === 2); //2 is strictly equal to 2 (have same type and value)
+console.log(2 === "2"); // false -2 (number) is strictly equal to 2 (A string)
+
+// strict not equal operator
+console.log(2 !== "2"); // true -2(number) is not strictly equal to 2 (A string)
+console.log(2 !== 2); // false   -2 (number) is not strictly equal to 2 (number)
